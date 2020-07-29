491cca4e8d01a42aa58d2adb7b448a446206be73
Aecio Silva <aeciofcs@gmail.com>
2020-07-29 10:18:29 -0300

    Aula03: Aplicando useState no cad de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 function CadastroCategoria(){
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
+
     return (
       <PageDefault>
-        <h1>Cadastrar Categoria</h1>
+        <h1>Cadastrar Categoria: {nomeDaCategoria}</h1>
+
+        <form onSubmit={function handleSubmit(event){
+          event.preventDefault();
+          setCategorias([
+            ...categorias,
+            nomeDaCategoria
+          ])
+        }}>
+          <label>
+            Nome da Categoria:
+            <input 
+              type="text"
+              value={nomeDaCategoria}
+              onChange={function funcaoHandler(event){
+                setNomeDaCategoria(event.target.value)
+              }}
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
+        <ul>
+          {categorias.map( (categoria, indice)=>{
+            return (
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+            )
+          } )}
+        </ul>
+    
         
         <Link to="/">
           Ir para a home
