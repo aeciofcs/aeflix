4622d2688b6eb21af5c6da11ac0f6700e3b04bdd
Aecio Silva <aeciofcs@gmail.com>
2020-07-28 22:32:47 -0300

    Aula02: Ajustes no cadastro e carrossel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,7 @@ function Home() {
       <BannerMain 
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é front-End? Trabalhando na área"}
+        videoDescription={"O que é front-End? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       />
 
       <Carousel 
